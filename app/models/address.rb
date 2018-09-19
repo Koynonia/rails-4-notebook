71372b7813a4b74a22229bb5dd33068694b00e98
EC2 Default User <ec2-user@ip-172-31-30-63.us-east-2.compute.internal>
2018-09-19 22:26:47 +0000

    060 - Finalizando a aplicacao

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,6 @@
 class Address < ActiveRecord::Base
   belongs_to :contact
   
-  validates :contact_id, presence: true
   validates :street, presence: true, length: { maximum: 50}
   validates :city, presence: true, length: { maximum: 50}
   validates :state, presence: true, length: { minimum: 2}
