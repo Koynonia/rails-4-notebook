4c32986134a19e4f75abafb4858f132192432add
EC2 Default User <ec2-user@ip-172-31-30-63.us-east-2.compute.internal>
2018-09-19 19:06:17 +0000

    057 - Uma pitada de segurança

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ActiveRecord::Base
   belongs_to :kind
-  has_one :address
+  has_one :address, :dependent => :destroy
   has_many :phones
   
   accepts_nested_attributes_for :address
